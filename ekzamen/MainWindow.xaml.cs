1ec7609967e2678ec66be54faab7fef8a55bba68
kolpakovEd <kolpakovedik737@gmail.com>
2024-12-23 08:32:35 +0300

    Ввод данных в приложении и обработка

--- ekzamen/MainWindow.xaml.cs
+++ ekzamen/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace ekzamen
     /// </summary>
     public partial class MainWindow : Window
     {
+        int[] temperatures;
         public MainWindow()
         {
             InitializeComponent();
@@ -27,7 +28,57 @@ namespace ekzamen
 
         private void CheckBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            int maxTemp = int.Parse(MaxTempTxt.Text);
+            int minTemp = int.Parse(MinTempTxt.Text);
+            int maxTempTime = int.Parse(OverheatTimeTxt.Text);
+            int minTempTime = int.Parse(OvercoldTimeTxt.Text);
+            DateTime shipmentDt = DateTime.Parse(ShipmentDateTxt.Text);
+            int OverheatTime = 0;
+            int OvercoldTime = 0;
+            DateTime CurTime = shipmentDt;
+            string[] temperatureStrings = TempChangeTxt.Text.Split(' ');
+            try
+            {
+                temperatures = temperatureStrings.Select(temp => int.Parse(temp.Trim())).ToArray();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Ошибка: вторая строка должна содержать целые числа");
+                return;
+            }
+            if (maxTemp < minTemp)
+            {
+                int buffer = maxTemp;
+                maxTemp = minTemp;
+                minTemp = buffer;
+                buffer = maxTempTime;
+                maxTempTime = minTempTime;
+                minTempTime = buffer;
+            }
+            string res = $"Дата отгрузки: {shipmentDt}\n";
+            foreach (int t in temperatures)
+            {
+                CurTime.AddMinutes(10);
+                if (t > maxTemp)
+                {
+                    OverheatTime += 10;
+                    res += $"Текущее время: {CurTime}; Текущее отклонение: {t - maxTemp}\n";
+                }
+                if (t < minTemp)
+                {
+                    OvercoldTime += 10;
+                    res += $"Текущее время: {CurTime}; Текущее отклонение: {t - minTemp}\n";
+                }
+                if (OvercoldTime > minTempTime)
+                {
+                    res += $"Текущее время: {CurTime}; Рыба была переохлаждена более {minTempTime} минут!\n";
+                }
+                if (OverheatTime > maxTempTime)
+                {
+                    res += $"Текущее время: {CurTime}; Рыба была перегрета более {maxTempTime} минут!\n";
+                }
+            }
+            ReportResTxt.Text = res;
         }
 
         private void LoadDataBtn_Click(object sender, RoutedEventArgs e)
@@ -36,6 +87,11 @@ namespace ekzamen
         }
 
         private void SaveReport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FishKindSel_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
